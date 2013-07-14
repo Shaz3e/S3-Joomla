1a37a6d1b339195602f209993cdc28f2309bcfa2
Shahrukh A. Khan <iam@shaz3e.com>
2013-07-14 05:54:16 +0500

    S3 Joomla v1.0c

--- layout/left.php
+++ layout/left.php
@@ -1,10 +1,10 @@
 <?php
     if($this->countModules('left')){
-        $modules["mod1"] = '<jdoc:include type="modules" name="left" style="s3_sidebar" />';
+        $leftModules["left"] = '<jdoc:include type="modules" name="left" style="s3_sidebar" />';
     }
-if(isset($modules)){
-    $result = count($modules);
-    foreach($modules as $module => $name){
+if(isset($leftModules)){
+    $result = count($leftModules);
+    foreach($leftModules as $leftModule => $name){
         if($result == 1){
             echo '<div class="dc-sidebar">';
             echo $name;

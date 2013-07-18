9adf83c0be0cc1a6afac81dedcc46ce8d00e414c
Shahrukh A. Khan <iam@shaz3e.com>
2013-07-18 23:54:05 +0500

    S3 Joomla v1.1

--- layout/component.php
+++ layout/component.php
@@ -1,1 +1,13 @@
-<?php s3Component(); ?>
+<?php
+	$app = JFactory::getApplication();
+	$menu = $app->getMenu();
+	$lang = JFactory::getLanguage();
+	if ($menu->getActive() == $menu->getDefault($lang->getTag())) {
+		if($this->params->get('showFrontPage')){
+			s3Component();
+		}
+	}else{
+		s3Component();
+	}
+
+?>

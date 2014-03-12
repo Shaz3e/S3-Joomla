9f26d2f9c7d4f48fce6c8ffdeaa18661bb898919
Shahrukh A. Khan <iam@shaz3e.com>
2014-03-12 03:13:38 -0700

    Edit cookie for one year

--- s3tools/S3StyleSwitcher.php
+++ s3tools/S3StyleSwitcher.php
@@ -3,6 +3,7 @@
 if(isset($_REQUEST["SETSTYLE"])){
 	if(setcookie("testcookie",true)){
 		setcookie("STYLE",$_REQUEST["SETSTYLE"],time()+3600,"/");
+		// setcookie("STYLE",$_REQUEST["SETSTYLE"],time()+31622400,"/"); for one year
 	} else {
 		$_SESSION["STYLE"]=$_REQUEST["SETSTYLE"];
 	}

502cd0a17015ebcc8281547a1a4c27e670cf94f6
Shahrukh A. Khan <iam@shaz3e.com>
2014-03-15 07:05:04 -0700

    Google Analytics Updated

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -16,15 +16,18 @@
 ?>
 <script type="text/javascript" src="<?php echo $dcTemplatePath;?>/js/jquery.min.js"></script>
 <jdoc:include type="head" />
+
+
 <?php include_once(JPATH_ROOT . "/templates/" . $this->template . '/s3tools/s3_styles.php'); ?>
 <script type="text/javascript" src="<?php echo $dcTemplatePath;?>/js/less.js"></script>
-<?php include_once(JPATH_ROOT . "/templates/" . $this->template . '/less/template_modules.php'); ?>
+
 <?php if($this->params->get('developmentMode')): ?>
 	<script type="text/javascript">
          less.env = "development";
          less.watch();
     </script>
 <?php endif; ?>
+
 <?php if($this->params->get('ResponsiveVideo')): ?>
 <script>
   $(document).ready(function(){
@@ -33,18 +36,24 @@
   });
 </script>
 <?php endif; ?>
+
+
+<?php include_once(JPATH_ROOT . "/templates/" . $this->template . '/less/template_modules.php'); ?>
+
 <?php
 // Google Analytics
 if($this->params->get('analytics')): ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '<?php echo $this->params->get('analytics');?>']);
+    _gaq.push(['_trackPageview']);
 
-  ga('create', '<?php echo $this->params->get('analytics');?>', '<?php echo $_SERVER['HTTP_HOST']; ?>');
-  ga('send', 'pageview');
-
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  
 </script>
 
 <?php endif; ?>

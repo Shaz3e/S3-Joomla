fbe22f2d92da89860bb6e4469992320592509097
Shahrukh A. Khan <iam@shaz3e.com>
2014-04-20 23:39:11 +0500

    modChrome Replace s3_block to s3_none

--- blocks/menu.php
+++ blocks/menu.php
@@ -1,21 +1,21 @@
 <?php
     if($this->countModules('menu-1')){
-        $menus["menu1"] = '<jdoc:include type="modules" name="menu-1" style="s3_block" />';
+        $menus["menu1"] = '<jdoc:include type="modules" name="menu-1" style="s3_none" />';
     }
     if($this->countModules('menu-2')){
-        $menus["menu2"] = '<jdoc:include type="modules" name="menu-2" style="s3_block" />';
+        $menus["menu2"] = '<jdoc:include type="modules" name="menu-2" style="s3_none" />';
     }
     if($this->countModules('menu-3')){
-        $menus["menu3"] = '<jdoc:include type="modules" name="menu-3" style="s3_block" />';
+        $menus["menu3"] = '<jdoc:include type="modules" name="menu-3" style="s3_none" />';
     }
     if($this->countModules('menu-4')){
-        $menus["menu4"] = '<jdoc:include type="modules" name="menu-4" style="s3_block" />';
+        $menus["menu4"] = '<jdoc:include type="modules" name="menu-4" style="s3_none" />';
     }
     if($this->countModules('menu-5')){
-        $menus["menu5"] = '<jdoc:include type="modules" name="menu-5" style="s3_block" />';
+        $menus["menu5"] = '<jdoc:include type="modules" name="menu-5" style="s3_none" />';
     }
     if($this->countModules('menu-6')){
-        $menus["menu6"] = '<jdoc:include type="modules" name="menu-6" style="s3_block" />';
+        $menus["menu6"] = '<jdoc:include type="modules" name="menu-6" style="s3_none" />';
     }
 if(isset($menus)){
     $result = count($menus);

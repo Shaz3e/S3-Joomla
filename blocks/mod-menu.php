60d930bfad576472799b13d0fdb39fd5d2c5b6fb
Shahrukh A. Khan <iam@shaz3e.com>
2014-04-20 21:18:09 +0500

    modChrome Added Shaz3e

--- blocks/mod-menu.php
+++ blocks/mod-menu.php
@@ -1,18 +1,18 @@
 <?php if($this->countModules('mod-menu-1')): ?>
-	<jdoc:include type="modules" name="mod-menu-1" style="s3_none" />
+	<jdoc:include type="modules" name="mod-menu-1" style="shaz3e" />
 <?php endif; ?>
 <?php if($this->countModules('mod-menu-2')): ?>
-	<jdoc:include type="modules" name="mod-menu-2" style="s3_none" />
+	<jdoc:include type="modules" name="mod-menu-2" style="shaz3e" />
 <?php endif; ?>
 <?php if($this->countModules('mod-menu-3')): ?>
-	<jdoc:include type="modules" name="mod-menu-3" style="s3_none" />
+	<jdoc:include type="modules" name="mod-menu-3" style="shaz3e" />
 <?php endif; ?>
 <?php if($this->countModules('mod-menu-4')): ?>
-	<jdoc:include type="modules" name="mod-menu-4" style="s3_none" />
+	<jdoc:include type="modules" name="mod-menu-4" style="shaz3e" />
 <?php endif; ?>
 <?php if($this->countModules('mod-menu-5')): ?>
-	<jdoc:include type="modules" name="mod-menu-5" style="s3_none" />
+	<jdoc:include type="modules" name="mod-menu-5" style="shaz3e" />
 <?php endif; ?>
 <?php if($this->countModules('mod-menu-6')): ?>
-	<jdoc:include type="modules" name="mod-menu-6" style="s3_none" />
+	<jdoc:include type="modules" name="mod-menu-6" style="shaz3e" />
 <?php endif; ?>

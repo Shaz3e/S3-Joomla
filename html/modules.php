24fba66cd3dee25ca24909b72cbad47b9e2f20ba
Shahrukh A. Khan <iam@shaz3e.com>
2014-04-20 21:17:18 +0500

    modChrome_s3_block added

--- html/modules.php
+++ html/modules.php
@@ -20,19 +20,27 @@
 defined('_JEXEC') or die;
 
 	function spanReplace($title){ 
-	$title = "<span>" . $title;
-	$space = strpos($title," ");
-	if($space){ 
-	$title = substr($title, 0, $space) . "</span>" . substr($title, $space);
-	}else{
-	$title .= "";
-	}
-	return $title;
+		$title = "<span>" . $title;
+		$space = strpos($title," ");
+			if($space){ 
+				$title = substr($title, 0, $space) . "</span>" . substr($title, $space);
+			}else{
+				$title .= "";
+			}
+		return $title;
 	}
 
 	function modChrome_s3_none($module, &$params, &$attribs)
 	{
 		echo $module->content;
+	}
+	
+	function modChrome_s3_block($module, &$params, &$attribs)
+	{
+		if (!empty ($module->content)) : ?>
+			<div class="dc-block <?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?>">
+				<?php echo $module->content; ?>
+			</div>
 	}
 
 	/* Default Module Style */

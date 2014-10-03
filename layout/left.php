40afbc2dc8d09a1f022577e1d8478504dd0d4ccc
Shahrukh A. Khan <iam@shaz3e.com>
2014-10-03 11:47:36 +0500

    Sidebar Code Structure Updated

--- layout/left.php
+++ layout/left.php
@@ -4,23 +4,10 @@
 		<div class="dc-modules">
 			<div id="dc-modules">
 				<div class="dc-clear"></div>
-					<?php
-                        if($this->countModules('left')){
-                            $leftModules["left"] = '<jdoc:include type="modules" name="left" style="s3_sidebar" />';
-                        }
-						
-						if(isset($leftModules)){
-							$result = count($leftModules);
-							foreach($leftModules as $leftModule => $name){
-								if($result == 1){
-									echo '<div class="dc-sidebar">';
-										echo $name;
-									echo '</div>';
-								}
-							}
-						}
-                    ?>
-				<div class="dc-clear"></div>
+	                <div class="dc-sidebar">
+						<jdoc:include type="modules" name="left" style="s3_sidebar" />
+					</div>
+                <div class="dc-clear"></div>
 			</div>
 		</div>
 	</div>
--- layout/right.php
+++ layout/right.php
@@ -4,22 +4,9 @@
     	<div class="dc-modules">
         	<div id="dc-modules">
             	<div class="dc-clear"></div>
-					<?php
-                        if($this->countModules('right')){
-                            $rightModules["right"] = '<jdoc:include type="modules" name="right" style="s3_sidebar" />';
-                        }
-						
-						if(isset($rightModules)){
-							$result = count($rightModules);
-							foreach($rightModules as $rightModule => $name){
-								if($result == 1){
-									echo '<div class="dc-sidebar">';
-										echo $name;
-									echo '</div>';
-								}
-							}
-						}
-                    ?>
+                	<div class="dc-sidebar">
+	                    <jdoc:include type="modules" name="right" style="s3_sidebar" />
+                    </div>
 				<div class="dc-clear"></div>
 			</div>
 		</div>

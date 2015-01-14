6a6a918c6b485a2c7a9bd3a1d2156b84e0dbb62a
Shahrukh A. Khan <iam@shaz3e.com>
2015-01-15 03:46:36 +0500

    Google Analytics Updated

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -140,18 +140,16 @@ if($this->params->get('ResponsiveVideo')): ?>
 <?php
 // Google Analytics
 if($this->params->get('analytics')): ?>
-	<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', '<?php echo $this->params->get('analytics');?>']);
-	_gaq.push(['_trackPageview']);
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+  ga('create', '<?php echo $this->params->get('analytics');?>', 'auto');
+  ga('send', 'pageview');
 
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-	</script>
+</script>
 <?php endif; ?>
 
 

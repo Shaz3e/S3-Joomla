f14cee6358f82f5e0f47f33e814a56fb1691121a
Shahrukh A. Khan <iam@shaz3e.com>
2015-02-06 02:10:53 +0500

    double quote fixed :+1:

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -113,7 +113,7 @@ if($this->params->get('fixedHeader')):
 <?php // Less Development Mode
 if($this->params->get('developmentMode')): ?>
 	<link rel="stylesheet/less" type="text/css" href="<?php echo $dcTemplatePath; ?>/themes/style<?php echo $this->params->get('style'); ?>/style.less">
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min.js""></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min.js"></script>
 	<?php if($this->params->get('LocalCDN')): ?>
 		<script type="text/javascript" src="<?php echo $dcTemplatePath; ?>/js/less.js"></script>
 	<?php else: ?>

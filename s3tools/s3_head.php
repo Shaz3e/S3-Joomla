9106e0397d734a9286a7e1b3571becd0f240c180
Shahrukh A. Khan <iam@shaz3e.com>
2015-07-30 23:31:36 +0500

    less.js Link Fixed

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -33,10 +33,10 @@ defined('_JEXEC') or die;
 <?php // Less Development Mode
 if($this->params->get('developmentMode')): ?>
   <link rel="stylesheet/less" type="text/css" href="<?php echo $dcTemplatePath; ?>/themes/style<?php echo $this->params->get('style'); ?>/style.less">
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min.js"></script>
   <?php if($this->params->get('LocalCDN')): ?>
-    <script type="text/javascript" src="<?php echo $dcTemplatePath; ?>/js/less.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/less.js/2.3.1/less.min.js"></script>
   <?php else: ?>
+    <script type="text/javascript" src="<?php echo $dcTemplatePath; ?>/js/less.js"></script>
   <?php endif; ?>
     
   <script type="text/javascript">

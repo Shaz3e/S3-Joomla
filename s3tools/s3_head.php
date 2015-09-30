529a795c14aabc338ead3d0c6e008ffd7d52bfea
Shahrukh A. Khan <iam@shaz3e.com>
2015-09-30 21:52:14 +0500

    html5.js v3.7.3 Updated

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -91,7 +91,7 @@ if($this->params->get('analytics')): ?>
 <!--[if lt IE 9]>
 	<link href="<?php echo $dcTemplatePath; ?>/css/ie.css" type="text/css" rel="stylesheet">
 <?php if($this->params->get('LocalCDN')): ?>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <?php else: ?>
 	<script type="text/javascript" src="<?php echo $dcTemplatePath; ?>/js/html5.js"></script>

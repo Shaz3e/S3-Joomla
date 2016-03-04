b4a0f35acca6511ffe1284d16efd4aa6bdb0a7e8
Shahrukh A. Khan <iam@shaz3e.com>
2016-03-04 20:21:41 +0500

    New Files Added

--- layout/component.php
+++ layout/component.php
@@ -18,7 +18,6 @@
 defined('_JEXEC') or die;
 	
 function s3Component(){ ?>
-
     <div id="page-content" class="block dc-page-content">
         <article>
             <jdoc:include type="message" />

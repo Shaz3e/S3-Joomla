140717a524319785e5b3af4ad46aab48afc2ff31
Shahrukh A. Khan <iam@shaz3e.com>
2016-03-11 02:04:30 +0500

    Analytics code updated

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -57,18 +57,10 @@ if($this->params->get('ResponsiveVideo')): ?>
 
 <?php
 // Google Analytics
-if($this->params->get('analytics')): ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', '<?php echo $this->params->get('analytics');?>', 'auto');
-  ga('send', 'pageview');
-
-</script>
-<?php endif; ?>
+if($this->params->get('analytics')):
+	echo $this->params->get('analytics');
+endif;
+?>
 
 <link rel="shortcut icon" href="<?php echo $dcTemplatePath; ?>/images/favicon/favicon.ico">
 <link rel="apple-touch-icon" sizes="57x57" href="<?php echo $dcTemplatePath; ?>/images/favicon/apple-touch-icon-57x57.png">

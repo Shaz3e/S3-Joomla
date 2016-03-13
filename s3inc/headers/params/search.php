b79f9554bc3ad2730614872eff556e8b22dbf6ed
Shahrukh A. Khan <iam@shaz3e.com>
2016-03-13 19:25:45 +0500

    code refactored

--- s3inc/headers/params/search.php
+++ s3inc/headers/params/search.php
@@ -7,21 +7,18 @@ defined('_JEXEC') or die;
  * @since S3 Framework 4.0
  */
 
-if( $this->params->get('s3_search_box') == 1){
-	echo '<div class="s3-search-box">';?>
+if( $this->params->get('s3_search_box') == 1): ?>
+	<div class="s3-search-box">
         <a href="#" id="search-open"><i class="fa fa-search"></i> Search</a>
         <div class="s3-popup" id="s3search">
-        <form id="searchform" method="post" action="<?php echo JRoute::_('index.php');?>">
-            <div class="form-group">
-	            <input type="search" name="searchword" id="mod-search-searchword" class="s3-search-input" placeholder="Search & Enter..." autocomplete="off">
-            </div>
-		<input type="hidden" name="task" value="search" />
-		<input type="hidden" name="option" value="com_search" />
-
-        </form>
-        	<a class="s3-search-close" href="#" id="search-close"><img src="<?php echo $dcTemplatePath; ?>/images/search-close.png"></a>
+            <form id="searchform" method="post" action="<?php echo JRoute::_('index.php');?>">
+                <div class="form-group">
+                <input type="search" name="searchword" id="mod-search-searchword" class="s3-search-input" placeholder="Search & Enter..." autocomplete="off">
+                </div>                
+            <input type="hidden" name="task" value="search" />
+            <input type="hidden" name="option" value="com_search" />
+            </form>
+            <a class="s3-search-close" href="#" id="search-close"><img src="<?php echo $dcTemplatePath; ?>/images/search-close.png"></a>
         </div>
-        <?php
-    echo '</div>'; // s3-search-box
-}
-?>
+	</div>
+<?php endif; ?>

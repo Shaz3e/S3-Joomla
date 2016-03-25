218a6a133124d1881c130fbf18d4c0d2d6cc84e6
Shahrukh A. Khan <iam@shaz3e.com>
2016-03-25 20:41:51 +0500

    6 New Header Styles Added

---
+++ s3inc/headers/header-14.php
@@ -0,0 +1,47 @@
+<?php
+	/**
+	 * Header Styles - Small Header RTLA (Right Alignment)
+	 * File Name: header-14.php
+	 * @since S3 Framework 4.0
+	 */
+
+// restricted access
+defined('_JEXEC') or die;
+?>
+<div class="dc-fixed-header">
+    <section class="dc-header dc-header-<?php echo $this->params->get('header_style'); ?> dc-clear" id="dc-header">
+        <div class="<?php echo( $this->params->get('fluidContainer') == 1 ? 'container-fluid' : 'row' ); ?>">
+            <div class="grid-<?php echo $this->params->get('logoGridClass'); ?>">
+                <div class="block">
+                    <div class="dc-logo">
+                    	<?php include_once('params/logo.php'); ?>
+                    </div>
+                </div>
+            </div><?php // .grid-3 ?>
+            
+            <?php if( $headerModules ): ?>
+            <div class="grid<?php echo $this->params->get('logoGridClass') - 12; ?>">
+            	<div class="block">
+                    <div class="dc-header-boxes">
+						<?php
+							include_once("params/phone.php");
+							include_once("params/email.php");
+							include_once("params/social.php");
+							include_once("params/search.php");
+							include_once("params/calltoaction.php");
+						?>
+                    </div>
+				</div>
+            </div>
+            <?php endif; ?>
+        </div><?php // .row ?>
+        
+        <div class="<?php echo( $this->params->get('fluidContainer') == 1 ? 'container-fluid' : 'row' ); ?>">
+        	<div class="col-1">
+                <div class="block">
+					<?php include_once("params/menu.php"); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
---
+++ s3inc/headers/header-18.php
@@ -0,0 +1,43 @@
+<?php
+	/**
+	 * Header Styles - Small Header &amp; Menu LTLA
+	 * File Name: header-18.php
+	 * @since S3 Framework 4.0
+	 */
+
+// restricted access
+defined('_JEXEC') or die;
+?>
+<div class="dc-fixed-header">
+    <section class="dc-header dc-header-<?php echo $this->params->get('header_style'); ?> dc-clear" id="dc-header">
+        <div class="<?php echo( $this->params->get('fluidContainer') == 1 ? 'container-fluid' : 'row' ); ?>">
+            <div class="grid-<?php echo $this->params->get('logoGridClass'); ?>">
+                <div class="block">
+                    <div class="dc-logo">
+                    	<?php include_once('params/logo.php'); ?>
+                    </div>
+                </div>
+            </div><?php // .grid-3 ?>
+            
+            <?php if( $headerModules ): ?>
+            <div class="grid<?php echo $this->params->get('logoGridClass') - 12; ?>">
+            	<div class="block">
+                    <div class="dc-header-boxes">
+						<?php
+							include_once("params/phone.php");
+							include_once("params/email.php");
+							include_once("params/social.php");
+							include_once("params/search.php");
+							include_once("params/calltoaction.php");
+						?>
+                        <div class="dc-clear"></div>
+                        <div class="dc-menu">
+                        	<?php include_once("params/menu.php"); ?>
+                        </div>
+                    </div>
+				</div>
+            </div>
+            <?php endif; ?>
+        </div><?php // .row ?>
+    </section>
+</div>
